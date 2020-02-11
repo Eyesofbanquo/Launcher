8e64bac143984cd3f8ee890abb793e0ce8a867c4
mshaw24 <37548798+mshaw24@users.noreply.github.com>
2020-02-11 11:01:29 -0600

    Import DisplayViewController

---
+++ Sources/Launcher/StandardModalTransitionDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Markim Shaw on 2/11/20.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+/// The standard transition delegate. All this does is present the view modally as intended without _any_ sort of transition animations
+class StandardModalTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
+  var grid: Grid?
+  var title: String?
+  
+  convenience init(grid: Grid, title: String? = nil) {
+    self.init()
+    self.title = title
+    self.grid = grid
+  }
+  
+  func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+    let presentationController = StandardModalPresentationController(presentedViewController: presented, presenting: presenting, grid: self.grid!, title: title)
+    return presentationController
+  }
+}
+
+#endif
